5a0dce7cd9347841634ed4d8884c937664ecf204
ASUS-CHA\denni\ddoichaboom <chahojun1004@gmail.com>
2025-07-29 18:03:45 +0900

    학습 - 포인터 크기, 자료형과 포인터 연산

---
+++ 250729/포인터 연산.cpp
@@ -0,0 +1,62 @@
+#pragma region 포인터의 크기
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int* p;
+//	cout << "64비트 운영체제" << sizeof(p) << endl;
+//
+//	return 0;
+//}
+
+#pragma endregion
+
+#pragma region 자료형과 포인터 연산의 관계성
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	short		sNum(1);
+//	int			iDst(10);
+//	float		fSrc(1.f);
+//	double		dTmp(1.0);
+//	long long	lEtc(10000);
+//
+//	short*		pNum = &sNum;
+//	int*		pDst = &iDst;
+//	float*		pSrc = &fSrc;
+//	double*		pTmp = &dTmp;
+//	long long*	pEtc = &lEtc;
+//
+//	cout << "-----자료형 <short> -----" << endl;
+//	cout << "sizeof(short) = " << sizeof(sNum) << "byte" << endl;
+//	cout << "pNum = " << pNum << '\t' << "pNum + 1 = " << pNum + 1 << endl;
+//
+//	cout << "-----자료형 <int> -----" << endl;
+//	cout << "sizeof(int) = " << sizeof(iDst) << "byte" << endl;
+//	cout << "pDst = " << pDst << '\t' << "pDst + 1 = " << pDst + 1 << endl;
+//
+//	cout << "-----자료형 <float> -----" << endl;
+//	cout << "sizeof(float) =" << sizeof(fSrc) << "byte" << endl;
+//	cout << "pSrc = " << pSrc << '\t' << "pSrc + 1 = " << pSrc + 1 << endl;
+//
+//	cout << "-----자료형 <double> -----" << endl;
+//	cout << "sizeof(double) =" << sizeof(dTmp) << "byte" << endl;
+//	cout << "pTmp = " << pTmp << '\t' << "pTmp + 1 = " << pTmp + 1 << endl;
+//
+//	cout << "-----자료형 <long long> -----" << endl;
+//	cout << "sizeof(long long) =" << sizeof(lEtc) << "byte" << endl;
+//	cout << "pEtc = " << pEtc << '\t' << "pEtc + 1 = " << pEtc + 1 << endl;
+//
+//
+//	return 0;
+//}
+
+#pragma endregion
+
