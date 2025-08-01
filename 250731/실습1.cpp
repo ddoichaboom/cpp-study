609c790c3b28a20a8a99e5a2282b59ab4489f78a
ASUS-CHA\denni\ddoichaboom <chahojun1004@gmail.com>
2025-08-01 12:42:57 +0900

    조건문이었던 기존 실습 코드 2중 for 문 조건으로 재작성

--- 250731/실습1.cpp
+++ 250731/실습1.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 void InitArray(int pArray[], int iSize);
-void PrintArray(int pArray[], int iSize);
+void PrintArray(int pArray[]);
 
 int main()
 {
 	int iArray[25] = { };
 
 	InitArray(iArray, size(iArray));
-	PrintArray(iArray, size(iArray));
+	PrintArray(iArray);
 
 	return 0;
 }
@@ -25,18 +25,35 @@ void InitArray(int pArray[], int iSize)
 	}
 }
 
-void PrintArray(int pArray[], int iSize)
+// ���ǹ� �̹Ƿ� 2�� for�� ���ǿ� ���� ����
+//void PrintArray(int pArray[], int iSize)
+//{
+//	for (int i = 0; i < iSize; ++i)
+//	{
+//		if (((i + 1) % 5) == 0)
+//		{
+//			cout << pArray[i] << endl;
+//		}
+//		else
+//		{
+//			cout << pArray[i] << "\t";
+//		}
+//	}
+//}
+
+// 2�� for�� 
+void PrintArray(int pArray[])
 {
-	for (int i = 0; i < iSize; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
-		if (((i + 1) % 5) == 0)
+		for (int j = 0; j < 5; ++j)
 		{
-			cout << pArray[i] << endl;
+			int index = i * 5 + j;
+
+			cout << pArray[index] << "\t";
+
 		}
-		else
-		{
-			cout << pArray[i] << "\t";
-		}
+		cout << endl;
 	}
 }
 
